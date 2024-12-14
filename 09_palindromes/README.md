a1ea800a596e9fb9c5c42be10e1d4275d5d16985
JasonEllsworth <jasonells48@icloud.com>
2024-12-14 17:02:45 +0800

    cant figure out how to capture the punctuation marker on the second test

--- 09_palindromes/README.md
+++ 09_palindromes/README.md
@@ -1,4 +1,4 @@
-# Exercise 09 - Palindromes
+0000# Exercise 09 - Palindromes
 
 Write a function that determines whether or not a given string is a palindrome.
 
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
 const palindromes = function () {
+    let args;
+    let reverse;
+    let punc;
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] === "!") {
+            punc = arguments[i];
+            console.log(punc);
+        } else {
+            args = arguments[i];
+            console.log(args);
+        }
 
+    }
+    reverse = args.split('').reverse().join('');
+
+    if (reverse === args) {
+        return true;
+    }
 };
 
 // Do not edit below this line
