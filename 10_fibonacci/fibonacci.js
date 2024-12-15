dac7b3ee66bacba2fb01686f7e20c57ec6dc044e
JasonEllsworth <jasonells48@icloud.com>
2024-12-15 15:55:57 +0800

    fib array is working well, all that is left is to ensure that a neg number isnt allowed and the logic can accept string values

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,22 @@
-const fibonacci = function() {
+const fibonacci = function(s) {
+   let fibarray = [0, 1, 1]
+    let newFibNum;
+   let j = 2;
+   let findNum = s;
+
+
+   //finished and working perfectly.
+    for (let i = 1; i < 99 ; i++) {
+        for (;;) {
+            newFibNum = fibarray[i] + fibarray[j];
+            fibarray.push(newFibNum);
+            j++;
+            break;
+        }
+    }
+
+    let arrayValue = fibarray[findNum];
+    return arrayValue;
 
 };
 
